7daf09fa5f96583e2779d659d3f7af5f16d59f3c
Burnercat <p70157015@gmail.com>
2025-01-10 13:00:54 +0530

    Delete a/banking directory

--- a/banking/Account.java
+++
@@ -1,9 +0,0 @@
- package banking;
- public interface Account {
-    void deposit(double amount);
-    void withdraw(double amount) throws InsufficientFundsException,
-    OverdraftLimitExceededException;
-    double checkBalance();
- }
- 
-
